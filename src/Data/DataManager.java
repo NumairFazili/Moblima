5a305c092bbfacb04ff58acbfe2a8aefcaa09e34
nomifaz <nomi.faz@gmail.com>
2019-10-31 13:13:41 +0800

    added booking class and data manager update

--- src/Data/DataManager.java
+++ src/Data/DataManager.java
@@ -1,5 +1,6 @@
 package Data;
 
+import Entity.Booking;
 import Entity.Cinema;
 import Entity.Movie;
 import com.google.common.base.Joiner;
@@ -140,7 +141,97 @@ public class DataManager {
             e.printStackTrace();
         }
     }
-//
+
+
+    public static ArrayList<Booking> LoadBookings(){
+        BufferedReader reader = null;
+        ArrayList<Booking> bookingArrayList=new ArrayList<>();
+        try {
+            reader=new BufferedReader(new FileReader("src/Data/"+"Booking"+".csv"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String line = "";
+        try {
+            reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                String[] tokens = line.split(",");
+
+
+                long bookingID=Long.parseLong(tokens[0]);
+                int cinplexID= Integer.parseInt(tokens[1]);
+                int cinemaID=Integer.parseInt(tokens[2]);
+                int movieID=Integer.parseInt(tokens[3]);
+                String showTime=tokens[4];
+                String cinemaClass=tokens[5];
+                String movieType=tokens[6];
+                String customerName=tokens[7];
+                long mobileNumber=Long.parseLong(tokens[8]);
+                String email=tokens[9];
+                String customerType=tokens[10];
+                int seatNO=Integer.parseInt(tokens[11]);
+                String bookingTime=tokens[12];
+                int price=Integer.parseInt(tokens[13]);
+
+
+
+                Booking booking=new Booking(bookingID,cinplexID,cinemaID,movieID,showTime,cinemaClass,movieType,customerName,mobileNumber,email,customerType,seatNO,bookingTime,price);
+                bookingArrayList.add(booking);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+        return bookingArrayList;
+    }
+
+
+    public static void AddBooking(Booking booking){
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter("src/Data/"+"Booking"+".csv",true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            writer.append(String.valueOf(booking.getBookingID()));
+            writer.append(",");
+            writer.append(String.valueOf(booking.getCinplexID()));
+            writer.append(",");
+            writer.append(String.valueOf(booking.getCinemaID()));
+            writer.append(",");
+            writer.append(String.valueOf(booking.getMovieID()));
+            writer.append(",");
+            writer.append(booking.getShowTime());
+            writer.append(",");
+            writer.append(booking.getCinemaClass());
+            writer.append(",");
+            writer.append(booking.getMovieType());
+            writer.append(",");
+            writer.append(booking.getCustomerName());
+            writer.append(",");
+            writer.append(String.valueOf(booking.getMobileNumber()));
+            writer.append(",");
+            writer.append(booking.getEmail());
+            writer.append(",");
+            writer.append(booking.getCustomerType());
+            writer.append(",");
+            writer.append(String.valueOf(booking.getSeatNO()));
+            writer.append(",");
+            writer.append(booking.getBookingTime());
+            writer.append(",");
+            writer.append(String.valueOf(booking.getPrice()));
+            writer.append("\n");
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     public static void main(String[] args){
@@ -163,7 +254,16 @@ public class DataManager {
 //        for(Cinema cinema:test2)
 //            System.out.println(cinema.getCinemaID());
 
-        AddShowTimes(new Cinema(2,2,1004,"11/10/2019 17:30",Arrays.asList(1,1,0,0)));
+//        AddShowTimes(new Cinema(2,2,1004,"11/10/2019 17:30",Arrays.asList(1,1,0,0)));
+
+
+//        ArrayList<Booking> test3=LoadBookings();
+//
+//        for(Booking booking:test3)
+//            System.out.println(booking.getBookingID());
+
+//        Booking booking=new Booking(15179,1,2,1002,"7/10/2019 16:30","Silver","2D","Test User 3",123456789,"xyz@gmail.com","student",12,"6/10/2019 16:30",11);
+//        AddBooking(booking);
 
 
     }
