7612a31a5d25439951e3890dc96ce1656dcf6a2e
lohQ <qiaoyanloh@gmail.com>
2019-11-03 11:20:03 +0800

    repush person, user, staff (user merged)

--- src/Entity/User.java
+++ src/Entity/User.java
@@ -1,42 +1,82 @@
 package Entity;
 
-public class User {
-    private String name,email;
-    private long mobileNumber;
+import java.util.ArrayList;
 
+public class User extends Person{
+    int age;
+    String name;
+    String email; 
+    String mobileNumber;
+//    ArrayList<Booking> bookings;
 
-    public User(String name, long mobileNumber, String email) {
+    //basic methods
+    public User(int age, String name, String email, String mobileNumber){
+        this.age = age;
         this.name = name;
         this.email = email;
         this.mobileNumber = mobileNumber;
+//        bookings = new ArrayList<Booking>();
     }
-
-
-
-    public String getName() {
+    void setAge(int age){
+        this.age = age;
+    }
+    void setName(String name) {
+        this.name = name;
+    }
+    void setEmail(String email){
+        this.email = email;
+    }
+    void setmobileNumber(String mobileNumber){
+        this.mobileNumber = mobileNumber;
+    }
+    int getAge(){
+        return age;
+    }
+    String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
+    String getEmail(){
         return email;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public long getMobileNumber() {
+    String getmobileNumber(){
         return mobileNumber;
     }
+    
+    public String toString() {
+        return "email\t:" + email + "\nage\t:" + String.valueOf(age) + "\nmobile\t:" + mobileNumber + "\n";
+    }
+    // end of basic methods
 
-    public void setMobileNumber(long mobileNumber) {
-        this.mobileNumber = mobileNumber;
+    /*
+    // MAY CONTAIN ERROR -- CAN'T TEST
+    void makeBooking(MovieShow m, int seatID){
+        Booking b = new Booking(m, seatID);
+        bookings.add(b);
     }
 
+    void cancelBooking(Booking b){
+        for(int i = bookings.size()-1; i >= 0; i--){
+            if(bookings.get(i) == b){
+                bookings.remove(i);
+                break;
+            }
+        }
+    }
 
+    void viewBookingHistory(){
+        for(int i = 0; i < bookings.size(); i++){
+            System.out.print(bookings.get(i));
+            //for this to work Booking object should have a toString method
+            //not using View class for now
+        }
+    }
 
+    void doReview(Movie m, String content, int rating){
+        m.addReview(content, rating);
+    }
+
+    void save(){
+        //Database.save(ID, age, email, mobileNumber, bookings);
+    }
+    */
 }
