0ba89099c76d87c92e1419857a799fbf0b7ea8ff
nomifaz <nomi.faz@gmail.com>
2019-11-04 17:05:27 +0800

    Added view class

---
+++ src/View/Boundary.java
@@ -0,0 +1,20 @@
+package View;
+
+public class Boundary {
+
+    public static void Display(String[] str){
+        int count=0;
+        for(int i=0;i<str.length;i++)
+            System.out.println(" "+String.valueOf(count++) + " : " + str[i]);
+        System.out.println(-1 + " : " + "Exit");
+    }
+
+//    public static void main(String[] args) {
+//
+//
+//        String[] str={"a","b","c","d","e"};
+//        Display(str);
+//
+//    }
+    }
+
