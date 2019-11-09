13aefd08e5f4f7f706e79a749041927366b08048
lohQ <qiaoyanloh@gmail.com>
2019-11-09 18:22:54 +0800

    added getAllShowTimes and editted getShowTimesByCineplex

--- src/Controller/PersonManager.java
+++ src/Controller/PersonManager.java
@@ -56,9 +56,19 @@ public class PersonManager{
         return SearchManager.get_topN_bySale(getAllMovies(), DataManager.LoadBookings());
     }
 
-    public List<Cinema> getShowTimesByCineplex(int cineplexID){
-        List<Cinema> c_list = DataManager.LoadShowTimes(cineplexID);
+    public List<Cinema> getAllShowTimes(){
+        List<Cinema> c_list = DataManager.LoadShowTimes(-1);
         return c_list;
+    }
+    public List<Cinema> getShowTimesByCineplex(int cineplexID){
+        List<Cinema> c_list = DataManager.LoadShowTimes(-1);
+        List<Cinema> return_list = new ArrayList<Cinema>();
+        for(Cinema c: c_list){
+            if(c.getCinplexID() == cineplexID){
+                return_list.add(c);
+            }
+        }
+        return return_list;
     }
     public List<Cinema> getShowTimesByMovie(int movieID){
         return DataManager.LoadShowTimes(movieID);
